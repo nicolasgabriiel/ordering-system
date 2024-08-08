a83da87e38616156bfaa43dc13c3e8720086c373
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-08-07 23:36:58 -0300

    Service layer, component registration

---
+++ src/main/java/com/nsilva/project/services/UserService.java
@@ -0,0 +1,27 @@
+package com.nsilva.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nsilva.project.entities.User;
+import com.nsilva.project.repositories.IUserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private IUserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
